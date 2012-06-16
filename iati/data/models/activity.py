9608cac8fa77c6231321f0009d78de23e36a64f3
intelliadmin <h.vanderheide@intellimedia.nl>
2012-06-16 12:10:20 +0200

    modelling sketches for iati compliants

--- iati/data/models/activity.py
+++ iati/data/models/activity.py
@@ -26,25 +26,25 @@ class IATIActivity(models.Model):
 
     activity_contact = models.ForeignKey(Contact, blank=True, null=True)
 
-    recipient_country = models.ForeignKey(Country) # **
-    recipient_region = models.ForeignKey(Region) # **
+    recipient_country = models.ForeignKey(Country, blank=True, null=True) # **
+    recipient_region = models.ForeignKey(Region, blank=True, null=True) # **
     # TODO: Sub-national Geographic Location
-    percentage = models.IntegerField() # **
+    percentage = models.IntegerField(blank=True, null=True) # **
 
     sector = models.ForeignKey(Sector)
 
-    collaboration_type = models.CharField(max_length=500, blank=True)
-    collaboration_type_code = models.CharField(max_length=50, blank=True)
-    default_flow_type = models.CharField(max_length=500, blank=True)
-    default_flow_type_code = models.CharField(max_length=50, blank=True)
-    default_aid_type = models.CharField(max_length=500, blank=True)
-    default_aid_type_code = models.CharField(max_length=50, blank=True)
-    default_finance_type = models.CharField(max_length=500, blank=True)
-    default_finance_code = models.CharField(max_length=50, blank=True)
-    default_tied_status = models.CharField(max_length=500, blank=True)
-    default_tied_status_code = models.CharField(max_length=50, blank=True)
-    activity_status = models.CharField(max_length=500, blank=True)
-    activity_status_code = models.CharField(max_length=50, blank=True)
+    collaboration_type = models.CharField(max_length=500, blank=True, null=True)
+    collaboration_type_code = models.CharField(max_length=50, blank=True, null=True)
+    default_flow_type = models.CharField(max_length=500, blank=True, null=True)
+    default_flow_type_code = models.CharField(max_length=50, blank=True, null=True)
+    default_aid_type = models.CharField(max_length=500, blank=True, null=True)
+    default_aid_type_code = models.CharField(max_length=50, blank=True, null=True)
+    default_finance_type = models.CharField(max_length=500, blank=True, null=True)
+    default_finance_code = models.CharField(max_length=50, blank=True, null=True)
+    default_tied_status = models.CharField(max_length=500, blank=True, null=True)
+    default_tied_status_code = models.CharField(max_length=50, blank=True, null=True)
+    activity_status = models.CharField(max_length=500, blank=True, null=True)
+    activity_status_code = models.CharField(max_length=50, blank=True, null=True)
 
     date_created = models.DateTimeField(auto_now_add=True, editable=False)
     date_updated = models.DateTimeField(auto_now=True, editable=False)
