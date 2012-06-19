54ee6714c265b119335b34d723ca18ec3a7af4ca
intelliadmin <h.vanderheide@intellimedia.nl>
2012-06-19 17:35:29 +0200

    changeset for OIPA API v2.0 implementation

--- iati/utils/admin.py
+++ iati/utils/admin.py
@@ -7,7 +7,7 @@ from utils.models import IATIXMLSource, Publisher
 
 
 class IATIXMLSourceAdmin(admin.ModelAdmin):
-    list_display = ['ref', 'publisher', 'type', 'local_file_exists', 'source_url', 'date_created', 'date_updated']
+    list_display = ['ref', 'publisher', 'type', 'local_file_exists', 'date_created', 'date_updated']
     list_filter = ('publisher', 'type')
 
 admin.site.register(Publisher)
